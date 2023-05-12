25147bb3c530424394113a4c542a647f156a75b3
InfernoDragon0 <mushroomapplepiele@gmail.com>
2023-05-12 21:40:17 +0800

    feat: add offline scene

--- COTL_Multiplayer/Helper/NetworkManagerHelper.cs
+++ COTL_Multiplayer/Helper/NetworkManagerHelper.cs
@@ -19,7 +19,7 @@ public static class NetworkManagerHelper
 
 
         managerScript.transport = steamworks;
-        managerScript.offlineScene = ""; //TODO find the main menu scene name
+        managerScript.offlineScene = "Main Menu"; //TODO find the main menu scene name
         managerScript.onlineScene = "Base Biome 1";
 
         networkManager.SetActive(true);
